2132a6ef3e28a4bf16559bd4ca79b9ba8beceee1
Oscar Vilca Rivera <oevr1997@gmail.com>
2020-12-10 17:12:35 -0500

    Hecho por Oscar Vilca Rivera

--- app/src/main/java/com/example/dam_07_cl/MainActivity.java
+++ app/src/main/java/com/example/dam_07_cl/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.dam_07_cl;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnlogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnlogin=findViewById(R.id.buttonlogin);
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(MainActivity.this, NotasActivity.class);
+                startActivity(i);
+
+            }
+        });
+
     }
 }
